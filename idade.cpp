8c687d91972da0c343a378463183adccbcdae984
04caap04 <68038486+04caap04@users.noreply.github.com>
2022-01-05 01:38:55 +0000

    Add files via upload

---
+++ idade.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ cout<< "Idade!\n";
+ 
+ int ano; //declarar a variavel;
+ cout << "indique o ano de nascimento: "; //indicar o ano de nascimento;
+ cin >>ano;
+ 
+ cout << "a idade e: "<<2021 - ano; //calcular a idade(ano de nascimento menos o ano actual);
+ 
+ return 0;
+}
